b241a62c6f2fc250e8a83530666a044f847f550b
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-11-26 07:17:20 +0530

    added Employee.js

--- Employee.js
+++ Employee.js
@@ -1,5 +1,5 @@
 //uc1
-const IS_ABSENT=0;
+/* const IS_ABSENT=0;
 
 let empCheck=Math.floor(Math.random()*10)%2;
 if(empCheck==IS_ABSENT)
@@ -9,9 +9,32 @@ if(empCheck==IS_ABSENT)
 else
 {
     console.log("Employee is present");
+ } */
+
+const IS_FULL_TIME=1;
+const IS_PART_TIME=2;
+const FULL_TIME_HOURS=8;
+const PART_TIME_HOURS=4;
+const WAGE_PER_HOUR=20;
+let emphr=0;
+let empcheck=Math.floor(Math.random()*10)%3
+console.log(empcheck)
+switch(empcheck)
+{
+    case IS_PART_TIME:
+        emphr=PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        emphr=FULL_TIME_HOURS;
+        break;
+    default:
+        emphr=0;
+        break;
+  
 }
-
-
+let dailywage=WAGE_PER_HOUR*emphr;
+console.log("daily wage of Employee Is"+dailywage);
+Footer
 
 
 
